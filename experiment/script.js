304d359027ca5b50b527b3b74aa528466543c889
Will Dickey <wfdickey@gmail.com>
2013-06-30 23:04:43 -0400

    initial commit

---
+++ experiment/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('#red').click(function() {
+        $('#red').fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+    $('#orange').click(function() {
+        $('#orange').fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+    $('#yellow').click(function() {
+        $('#yellow').fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+    $('#green').click(function() {
+        $('#green').fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+    $('#blue').click(function() {
+        $('#blue').fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+    $('#purple').click(function() {
+        $('#purple').fadeOut('slow');
+    });
+});

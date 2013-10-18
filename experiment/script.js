f3746fd94fe080889738e27ac5df6590c7aa9061
Will Dickey <wfdickey@gmail.com>
2013-10-18 16:54:43 -0400

    new stuff

--- experiment/script.js
+++ experiment/script.js
@@ -1,71 +1,8 @@
 $(document).ready(function() {
-    $('#red').click(function() {
-        $('#red').fadeOut('slow');
+    $('div').mouseover(function() {
+        $(this).fadeTo(5000,'0');
     });
 });
 
-$(document).ready(function() {
-    $('#orange').click(function() {
-        $('#orange').fadeOut('slow');
-    });
-});
 
-$(document).ready(function() {
-    $('#yellow').click(function() {
-        $('#yellow').fadeOut('slow');
-    });
-});
 
-$(document).ready(function() {
-    $('#green').click(function() {
-        $('#green').fadeOut('slow');
-    });
-});
-
-$(document).ready(function() {
-    $('#blue').click(function() {
-        $('#blue').fadeOut('slow');
-    });
-});
-
-$(document).ready(function() {
-    $('#purple').click(function() {
-        $('#purple').fadeOut('slow');
-    });
-});
-
-$(document).ready(function() {
-    $('#red1').click(function() {
-        $('#red1').fadeOut('slow');
-    });
-});
-
-$(document).ready(function() {
-    $('#orange1').click(function() {
-        $('#orange1').fadeOut('slow');
-    });
-});
-
-$(document).ready(function() {
-    $('#yellow1').click(function() {
-        $('#yellow1').fadeOut('slow');
-    });
-});
-
-$(document).ready(function() {
-    $('#green1').click(function() {
-        $('#green1').fadeOut('slow');
-    });
-});
-
-$(document).ready(function() {
-    $('#blue1').click(function() {
-        $('#blue1').fadeOut('slow');
-    });
-});
-
-$(document).ready(function() {
-    $('#purple1').click(function() {
-        $('#purple1').fadeOut('slow');
-    });
-});

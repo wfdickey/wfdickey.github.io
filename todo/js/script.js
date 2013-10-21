4344c0c08841bdd8aead31563cb8cb264879ab04
Will Dickey <wfdickey@gmail.com>
2013-10-20 20:28:33 -0400

    changed alot

--- todo/js/script.js
+++ todo/js/script.js
@@ -1,9 +1,28 @@
+/* Checking for local storage */
+if(typeof(localStorage))
+
+
 $(document).ready(function() {
+	/* Click submit button to add item to list */ 
     $("#button").click(function() {
         var toAdd = $("input[name=checkListItem]").val();
-        $(".list").append($("<div class='item'>" + toAdd + "</div>"));
+        $(".list").append($("<a href='#'><div class='item'>" + toAdd + "</div></a>"));
+
     });
+
+    /*Hit enter to click submit button*/
+ 	$("form").keypress(function(e){
+        if(e.which == 13){//Enter key pressed
+            $("#button").click();//Trigger search button click event
+        }
+    });
+
+ 	 /*Click Item to remove it*/
     $(document).on("click", ".item",  function(){
         $(this).remove();
     });
 }); 
+
+
+
+

c4f7bcc96e2958229d37ba934c277035f09d352f
Will Dickey <wfdickey@gmail.com>
2013-10-22 11:23:27 -0400

    fixed plugin

--- fts/sprint4/js/addressbook.js
+++ fts/sprint4/js/addressbook.js
@@ -6,7 +6,7 @@
 
         //define default options 
         var defaults = {
-            filePath: "data/contacts.json"
+            filePath: "data/contacts.json",
             outputId: "#output"
 
         };
@@ -35,7 +35,7 @@
                             count = addrBook.length;
                         
                         // clear the target area just incase there's something in it.
-                        $(outputId).empty();
+                        $(options.outputId).empty();
                         
                         // check the count, of course
                         if (count > 0 && searchValue !== "") {
@@ -49,7 +49,7 @@
                                 // anything other than -1 means we found a match
                                 if(isItFound !== -1) {
                                 
-                                    $(outputId).append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>');
+                                    $(options.outputId).append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>');
                                 
                                 } // end if
                                 
@@ -63,13 +63,13 @@
                 getAllContacts : function () {
                     
                     // start Ajax call
-                    $.getJSON(filePath, function (data) {
+                    $.getJSON(options.filePath, function (data) {
                         
                         var addrBook = data.addressBook,
                             count = addrBook.length;
                         
                         // clear the target area just incase there's something in it.
-                        $(outputId).empty();
+                        $(options.outputId).empty();
                         
                         // check the count, of course
                         if (count > 0) {
@@ -77,7 +77,7 @@
                             // loop through the contacts
                             $.each(addrBook, function (i, obj) {
                                
-                                $(outputId).append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>');
+                                $(options.outputId).append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>');
                             
                             }); // end each
                         
@@ -122,113 +122,7 @@
                
             $("#get-all").click(addr.getAllContacts);
 
-        })();
+        });
     };
 })(jQuery);
 
-(function(){
-
-    /* define address book methods */
-    var addr = {
-        
-        search : function (event) {
-            
-            // stop the default behavior
-            event.preventDefault();
-            
-            $.getJSON('data/contacts.json', function (data) {
-                
-                // save the input value, contacts length and i to variables
-                var searchValue = $("#q").val(),
-                    addrBook = data.addressBook,
-                    count = addrBook.length;
-                
-                // clear the target area just incase there's something in it.
-                $('#output').empty();
-                
-                // check the count, of course
-                if (count > 0 && searchValue !== "") {
-                    
-                    // loop through the contacts
-                    $.each(addrBook, function (i, obj) {
-                        
-                        // look through the name value to see if it contains the searchterm string    
-                        var isItFound = obj.name.indexOf(searchValue);
-                        
-                        // anything other than -1 means we found a match
-                        if(isItFound !== -1) {
-                        
-                            $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>');
-                        
-                        } // end if
-                        
-                    }); // end each loop
-                
-                } // end count check
-                
-            }); // end ajax call
-        
-        },
-        getAllContacts : function () {
-            
-            // start Ajax call
-            $.getJSON('data/contacts.json', function (data) {
-                
-                var addrBook = data.addressBook,
-                    count = addrBook.length;
-                
-                // clear the target area just incase there's something in it.
-                $('#output').empty();
-                
-                // check the count, of course
-                if (count > 0) {
-                    
-                    // loop through the contacts
-                    $.each(addrBook, function (i, obj) {
-                       
-                        $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">'+ obj.email +'</a><p>');
-                    
-                    }); // end each
-                
-                } // end count check
-            
-            }); // end ajax call
-        }
-    
-    }; // end addr object
-    
-    /*
-        All events attached to the searchbox
-    */
-    
-    $("#q").keyup(addr.search).focus(function () {
-        
-        $(this).parent().addClass("active");
-        
-    }).blur(function () {
-    
-        $(this).parent().removeClass("active");
-    
-    });
-    
-    /*
-        All events attached to the search form
-    */
-    
-    $("#search-form").hover(function () {
-        
-        $(this).addClass("hovering");
-        
-    }, function () {
-    
-        $(this).removeClass("hovering");
-    
-    }).submit(addr.search);
-    
-    /*
-        Get all contacts action
-    */
-       
-    $("#get-all").click(addr.getAllContacts);
-
-})();
